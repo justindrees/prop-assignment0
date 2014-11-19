f759a8d3997e5c52b1993a109a5bb1b1659151a1
perny_000 <perny_000@Per>
2014-11-19 14:02:12 +0100

    more of the same

--- src/prop/assignment0/Parser.java
+++ src/prop/assignment0/Parser.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 public class Parser implements IParser {
 	
 	Tokenizer tn = new Tokenizer();
-	ArrayList<INode> parseTree = new ArrayList<INode>();
+	ArrayList<Lexeme> parseTree = new ArrayList<Lexeme>();
 	
 	
 	public Parser() {	
@@ -20,24 +20,75 @@ public class Parser implements IParser {
 	// Parses a program from file returning a parse tree (the root node of a parse tree).
 	@Override
 	public INode parse() throws IOException, TokenizerException, ParserException {		// use tn.current() and tn.moveNext()
+		tn.moveNext();
 		INode n = assign();
 		return n;
 	}
-	private INode assign(){
-		// assign = id,=,expr,;
-		return new AssignmentNode(tn);
+	private AssignmentNode assign() throws IOException, TokenizerException{
+		// assign = id , = , expr ,;
+		if(tn.current().token() == Token.IDENT){
+			parseTree.add(tn.current());
+			tn.moveNext();
+			if(tn.current().token() == Token.ASSIGN_OP){
+				parseTree.add(tn.current());
+				tn.moveNext();
+				INode n = expr();
+				//parseTree.add(/*lexeme?*/)
+				if(tn.current().token() == Token.SEMICOLON){
+					return n;
+				}
+			}
+		}
+		throw new ParserException("Syntax error. ParserException in assign() ");
 	}
-	private INode expr(){
+	private ExpressionNode expr() throws IOException, TokenizerException{
 		// expr = term, + || -, expr
-		return new ExpressionNode();
+		tn.moveNext();
+		INode n = term();
+		if(tn.current().token() == Token.ADD_OP){
+			parseTree.add(tn.current());
+			tn.moveNext();
+		}
+		if(tn.current().token() == Token.SUB_OP){
+			parseTree.add(tn.current());
+			tn.moveNext();
+		}
+		INode n2 = expr();
+		return n2;
+		throw new ParserException("Syntax error. ParserException in expr() ");
 	}
-	private INode term(){
+	private TermNode term() throws IOException, TokenizerException{
 		// term = factor, * | /, term
-		return new TermNode();
+		INode n = factor();
+		if(tn.current().token() == Token.MULT_OP){
+			parseTree.add(tn.current());
+			tn.moveNext();
+		}
+		if(tn.current().token() == Token.DIV_OP){
+			parseTree.add(tn.current());
+			tn.moveNext();
+		}
+		return n;
+		throw new ParserException("Syntax error. ParserException in term() ");
 	}
-	private INode factor(){
+	private FactorNode factor() throws IOException, TokenizerException{
 		// factor = int | (, expr ,)
+		if(tn.current().token() == Token.INT_LIT){
+			parseTree.add(tn.current());
+			tn.moveNext();
+		}else if(tn.current().token() == Token.LEFT_PAREN){
+			parseTree.add(tn.current());
+			tn.moveNext();
+			INode n = expr();
+			if(tn.current().token() == Token.RIGHT_PAREN){
+				parseTree.add(tn.current());
+				tn.moveNext();
+			}
+			
+		}
+		
 		return new FactorNode();
+		throw new ParserException("Syntax error. ParserException in factor() ");
 	}
 	// Closes the file and releases any system resources associated with it.
 	@Override
