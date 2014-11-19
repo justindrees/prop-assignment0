8fa37cf03de8fb9f0abdf149c22858598d41c66f
perny_000 <perny_000@Per>
2014-11-19 12:26:34 +0100

    more parser and tokenizer fixes and additions

--- src/prop/assignment0/Parser.java
+++ src/prop/assignment0/Parser.java
@@ -1,29 +1,28 @@
 package prop.assignment0;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class Parser implements IParser {
 	
 	Tokenizer tn = new Tokenizer();
+	ArrayList<INode> parseTree = new ArrayList<INode>();
 	
-	public Parser() {
-		
+	
+	public Parser() {	
 	}
-	
 	// Opens a file for parsing.
 	@Override
 	public void open(String fileName) throws IOException, TokenizerException {
 		Tokenizer tokenizer = new Tokenizer();
 		tokenizer.open(fileName);
 	}
-
 	// Parses a program from file returning a parse tree (the root node of a parse tree).
 	@Override
-	public INode parse() throws IOException, TokenizerException, ParserException {
+	public INode parse() throws IOException, TokenizerException, ParserException {		// use tn.current() and tn.moveNext()
 		INode n = assign();
 		return n;		
 	}
-	
 	private INode assign(){
 		// assign = id,=,expr,;
 		return new AssignmentNode(tn);
